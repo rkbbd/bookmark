4558081e45e58558d7731040af593b72231fe22a
MD RAKIB HASAN <rakib424.com>
2023-12-28 00:53:19 +0600

    add download

--- BookMarkApp/BookMarkApp/Controllers/HomeController.cs
+++ BookMarkApp/BookMarkApp/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.IO;
 using System.Net;
 using System.Text;
 using System.Xml.Linq;
@@ -19,81 +20,17 @@ namespace BookMarkApp.Controllers
 
         public async Task<IActionResult> Index()
         {
+            var books = new List<Book>();
             var url = "https://www.rokomari.com/book/15991/bangali-musulmaner-mon";
-            //using (var client = new HttpClient())
-            //{
-            //    // We'll use the GetAsync method to send  
-            //    // a GET request to the specified URL 
-            //    var response = await client.GetAsync(url);
+            var book = await BookInfo.GetBookDetails(url);
+            books.Add(book);
+            var bytes = BookInfo.makeExcel(books);
 
-            //    // If the response is successful, we'll 
-            //    // interpret the response as XML 
-            //    if (response.IsSuccessStatusCode)
-            //    {
-            //        var xml = await response.Content.ReadAsStringAsync();
 
-            //        // We can then use the LINQ to XML API to query the XML 
-            //        var doc = XDocument.Parse(xml);
-
-            //        // Let's query the XML to get all of the <title> elements 
-            //        var titles = from el in doc.Descendants("title")
-            //                     select el.Value;
-
-            //        // And finally, we'll print out the titles 
-            //        foreach (var title in titles)
-            //        {
-            //            Console.WriteLine(title);
-            //        }
-            //    }
-            //}
-           // string url = "http://website.com";
-            var Webget = new HtmlWeb();
-            var doc = Webget.Load(url);
-            //foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//table"))
-            //{
-            //    var j = node;
-            //   // names.Add(node.ChildNodes[0].InnerHtml);
-            //}
-            //foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//li[@class='tel']//a"))
-            //{
-            //    var node1 = node;
-            //   // phones.Add(node.ChildNodes[0].InnerHtml);
-            //}
-            var cols = doc.DocumentNode.SelectNodes("//table[@class='table table-bordered']//tr//td");
-            for (int i = 0; i < cols.Count; i = i + 2)
-            {
-                string name = cols[i].InnerText.Trim();
-                string va = cols[i + 1].InnerText;
-            }
-            var price = doc.DocumentNode.SelectNodes("//div[@class='details-book-info__content-book-price']//strike[@class='original-price']"); //price
-            for (int i = 0; i < price.Count; i = i + 2)
-            {
-                string name2 = price[i].InnerText.Trim();
-                //string va = price[i + 1].InnerText;
-            }
-            var sellPrice = doc.DocumentNode.SelectNodes("//div[@class='details-book-info__content-book-price']//span[@class='sell-price']"); //price
-            string se = sellPrice[0].InnerText.Trim();
-
-            var discount = doc.DocumentNode.SelectNodes("//div[@class='details-book-info__content-book-price']//span[@class='js--save-message']"); //price
-            string d = discount[0].InnerText.Trim();
-
-            var category = doc.DocumentNode.SelectNodes("//div[@class='details-book-info__content-category d-flex align-items-center']//a"); //price
-            string c = category[0].InnerText.Trim();
-            string href = category[0].Attributes["href"].Value;
-
-            var bookImg = doc.DocumentNode.SelectNodes("//div//div//div//div//div[@class='image-container']//img"); //price
-            var img = bookImg[0].Attributes["src"].Value;
-
-            var bookSummary = doc.DocumentNode.SelectNodes("//div[@id='js--summary-description']"); //price
-            var summary = bookSummary[0].InnerText;
-
-            var authorName = doc.DocumentNode.SelectNodes("//div[@class='col author_des']//p"); //price
-            var aname = authorName[0].InnerText;
-
-            var author = doc.DocumentNode.SelectNodes("//div[@class='col author_des']//div"); //price
-            var authorDesc = author[0].InnerText;
-
-            return View();
+            var content = new System.IO.MemoryStream(bytes);
+            var contentType = "APPLICATION/octet-stream";
+            var fileName = "something.csv";
+            return File(content, contentType, fileName);
         }
 
         public IActionResult Privacy()
---
+++ BookMarkApp/BookMarkApp/Models/BookInfo.cs
@@ -0,0 +1,130 @@
+﻿using HtmlAgilityPack;
+using System.Dynamic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Text;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace BookMarkApp.Models
+{
+    internal static class BookInfo
+    {
+        public static async Task<Book> GetBookDetails(string url)
+        {
+            var Webget = new HtmlWeb();
+            var doc = await Webget.LoadFromWebAsync(url);
+            var book = new Book();
+            var cols = doc.DocumentNode.SelectNodes("//table[@class='table table-bordered']//tr//td");
+            for (int i = 0; i < cols.Count; i = i + 2)
+            {
+                string name = string.Join("", cols[i].InnerText.Trim().Split(default(string[]), StringSplitOptions.RemoveEmptyEntries));
+                string value = cols[i + 1].InnerText;
+                book.SetValue(name, value);
+            }
+            book.Price = doc.DocumentNode.SelectNodes("//div[@class='details-book-info__content-book-price']//strike[@class='original-price']")[0].InnerText.Trim();
+
+            book.SellPrice = doc.DocumentNode.SelectNodes("//div[@class='details-book-info__content-book-price']//span[@class='sell-price']")[0].InnerText.Trim();
+
+            book.Discount = doc.DocumentNode.SelectNodes("//div[@class='details-book-info__content-book-price']//span[@class='js--save-message']")[0].InnerText.Trim();
+
+            var category = doc.DocumentNode.SelectNodes("//div[@class='details-book-info__content-category d-flex align-items-center']//a");
+            book.Category = category[0].InnerText.Trim();
+            book.CategoryLink = string.Concat(url.Split("com")[0], "com",category[0].Attributes["href"].Value);
+
+            book.BookImg = doc.DocumentNode.SelectNodes("//div//div//div//div//div[@class='image-container']//img")[0].Attributes["src"].Value;
+
+            book.Summary = doc.DocumentNode.SelectNodes("//div[@id='js--summary-description']")[0].InnerText;
+
+            //var authorName = doc.DocumentNode.SelectNodes("//div[@class='col author_des']//p")[0].InnerText;
+
+            book.AuthorDescription = doc.DocumentNode.SelectNodes("//div[@class='col author_des']//div")[0].InnerText;
+
+            return book;
+        }
+
+        public static byte[] makeExcel(List<Book> books)
+        {
+            MemoryStream ms = new MemoryStream();
+            using (StreamWriter sw = new StreamWriter(ms, Encoding.UTF8))
+            {
+                var stringCSV = ConvertToCsv(books);
+                sw.WriteLine(stringCSV);
+                sw.Flush();
+            }
+            byte[] bytes = ms.ToArray();
+            ms.Close();
+            return bytes;
+        }
+        static string ConvertToCsv<T>(IEnumerable<T> data)
+        {
+            StringWriter csvString = new StringWriter();
+
+            // Write the header
+            csvString.WriteLine(string.Join(",", typeof(T).GetProperties().Select(prop => QuoteIfNeeded(prop.Name))));
+
+            // Write the data
+            foreach (var item in data)
+            {
+                var values = typeof(T).GetProperties()
+                    .Select(prop => QuoteIfNeeded(prop.GetValue(item)?.ToString() ?? ""));
+                csvString.WriteLine(string.Join(",", values));
+            }
+
+            return csvString.ToString();
+        }
+
+
+        static string QuoteIfNeeded(string value)
+        {
+            if (value.Contains(',') || value.Contains('"'))
+            {
+                // Escape double quotes by doubling them and enclose the value in double quotes
+                return $"\"{value.Replace("\"", "\"\"")}\"";
+            }
+            else
+            {
+                return value;
+            }
+        }
+        public static void SetValue<T>(this T sender, string propertyName, object value)
+        {
+            var propertyInfo = sender.GetType().GetProperty(propertyName);
+
+            if (propertyInfo is null) return;
+
+            var type = Nullable.GetUnderlyingType(propertyInfo.PropertyType) ?? propertyInfo.PropertyType;
+
+            if (propertyInfo.PropertyType.IsEnum)
+            {
+                propertyInfo.SetValue(sender, Enum.Parse(propertyInfo.PropertyType, value.ToString()!));
+            }
+            else
+            {
+                var safeValue = (value == null) ? null : Convert.ChangeType(value, type);
+                propertyInfo.SetValue(sender, safeValue, null);
+            }
+        }
+
+    }
+    public class Book
+    {
+        public string BookImg { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string Author { get; set; }
+        public string AuthorDescription { get; set; }
+        public string Translator { get; set; }
+        public string Price { get; set; }
+        public string SellPrice { get; set; }
+        public string Discount { get; set; }
+        public string NumberofPages { get; set; }
+        public string Publisher { get; set; }
+        public string Category { get; set; }
+        public string ISBN { get; set; }
+        public string Edition { get; set; }
+        public string Country { get; set; }
+        public string Language { get; set; }
+        public string CategoryLink { get; set; }
+        public string Tag { get; set; }
+    }
+}
